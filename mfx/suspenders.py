89c538a150f93fc302d853b699cae576be186f49
Zachary Lentz <zlentz@slac.stanford.edu>
2018-01-30 16:22:15 -0800

    ENH: simplify suspenders file

--- mfx/suspenders.py
+++ mfx/suspenders.py
@@ -1,12 +1,9 @@
-import logging
 import threading
 
 import numpy as np
 
+from bluesky.suspenders import SuspendFloor
 from ophyd.signal import Signal, EpicsSignalRO
-from ophyd.suspenders import SuspenderBase, SuspendFloor
-
-logger = logging.getLogger()
 
 
 class AvgSignal(Signal):
@@ -30,39 +27,16 @@ class AvgSignal(Signal):
             self.put(np.mean(self.values))
 
 
-class PvSuspenderBase(SuspenderBase):
-    """
-    Base class for a suspender that expects a pvname instead of a signal.
-    If averages is greater than 1, we'll implement a rolling average of the
-    signal instead of the raw value.
-    """
-    def __init__(self, pvname, sleep=0, pre_plan=None, post_plan=None,
-                 tripped_message="", averages=1, **kwargs):
-        sig = EpicsSignalRO(pvname)
-        if averages > 1:
-            sig = AvgSignal(sig, averages, name=sig.name + "_avg")
-
-        super().__init__(sig, sleep=sleep, pre_plan=pre_plan(),
-                         post_plan=post_plan(),
-                         tripped_message=tripped_message,
-                         **kwargs)
-
-
-class PvSuspendFloor(SuspendFloor, PvSuspenderBase):
-    """
-    Suspend the run if a pv falls below a set value.
-    """
-    pass
-
-
-class BeamEnergySuspendFloor(PvSuspendFloor):
+class BeamEnergySuspendFloor(SuspendFloor):
     """
     Suspend the run if the beam energy falls below a set value.
     """
     def __init__(self, suspend_thresh, resume_thresh=None, sleep=5.0,
-                 pre_plan=None, post_plan=None, averages=120,
-                 **kwargs):
-        super().__init__("GDET:FEE1:241:ENRC", suspend_thresh,
-                         resume_thresh=resume_thresh, sleep=sleep,
-                         pre_plan=pre_plan, post_plan=post_plan,
-                         averages=averages, **kwargs)
+                 averages=120, **kwargs):
+
+        sig = EpicsSignalRO('GDET:FEE1:241:ENRC')
+        if averages > 1:
+            sig = AvgSignal(sig, averages, name=sig.name + "_avg")
+
+        super().__init__(sig, suspend_thresh, resume_thresh=resume_thresh,
+                         sleep=sleep, **kwargs)
