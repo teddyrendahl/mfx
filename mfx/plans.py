35c111485c848f158c93b07f7a1444d1b6c32f40
teddyrendahl <trendahl@slac.stanford.edu>
2018-03-08 15:13:09 -0800

    Remove deprecated DAQ calls from transfocator scan

--- mfx/plans.py
+++ mfx/plans.py
@@ -1,11 +1,8 @@
 from bluesky.plan_stubs import open_run, mv, close_run
-
-from hutch_python import hutch
 from .beamline import tfs_trans
 
 
-def tfs_scan(start, stop, min_energy=None):
-    hutch.daq.configure(always_on=True)
+def tfs_scan(start, stop):
     yield from open_run()
     yield from mv(tfs_trans, start)
     yield from mv(tfs_trans, stop)
